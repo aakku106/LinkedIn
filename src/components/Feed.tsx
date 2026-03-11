a02798a2801e5dfeaf0b3881e1f0d25204983f55
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-11 14:08:55 +0545

    moove feef to components

---
+++ src/components/Feed.tsx
@@ -0,0 +1,29 @@
+import "../features/feed/usePosts"
+import { Posts } from "./Posts"
+
+export default function Feed() {
+
+  return (
+    <div className=" feed-page flex flex-row flex-wrap px-56 mt-4 relative">
+
+      <div className=" left-feed fixed min-w-52 left-32 mr-6 p-6 bg-black">
+        left
+
+        <div className="profile bg-black/95 dark:bg-blue-100">
+          fffff
+        </div>
+        <div className="data"></div>
+        <div className="other-nav"></div>
+
+      </div>
+      <div className=" center-feed  min-w-136 max-w[34rem]  ml-44 ">
+
+        <Posts />
+
+      </div>
+      <div className="right-feed fixed  min-w-64 right-48 p-6 bg-blue-800">
+        Right
+      </div>
+    </div>
+  )
+}
--- src/features/feed/FeedPage.tsx
+++ src/features/feed/FeedPage.tsx
@@ -1,30 +1,6 @@
-import { usePosts } from "./usePosts"
-import "../../assets/styles/feedPage.css"
+import Feed from "../../components/Feed"
 
 export default function FeedPage() {
-  const posts = usePosts()
-
-  if (!posts) return <p>Loading...</p>
-
-  return (
-    <div className=" feed-page flex flex-row px-56 mt-4 relative">
-
-      <div className=" left-feed fixed size-52 left-32 mr-6 p-6 bg-amber-600">
-        left
-      </div>
-      <div className=" center-feed  min-w-136 max-w[34rem]  ml-44 ">
-
-        {posts.map((post) => (
-          <div key={post.id} className="bg-white mt-4 ">
-            <p>{post.content}</p>
-          </div>
-        ))}
-
-      </div>
-      <div className="right-feed fixed  size-64 right-48 p-6 bg-blue-800">
-        Right
-      </div>
-    </div>
-  )
+  return <Feed />
 }
 
