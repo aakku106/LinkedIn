4f4ef655c633ae8a714041c7c7a009daa27cddc8
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-11 09:05:47 +0545

    used navlink insted of div in logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,21 +6,13 @@ export default function Navbar() {
     <nav className=" sticky top-0 backdrop-blur-sm navBar flex justify-between bg-white/90 dark:bg-black/90 text-black dark:text-white text-nowrap items-center-safe " >
 
 
-      <div className="logo-container order-first flex flex-col items-center-safe hover:cursor-pointer">
+      <NavLink
+        to={"/"}
+        className="logo-container order-first flex flex-col items-center-safe hover:cursor-pointer hover:text-blue-700}">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 34 34" data-supported-dps="34x34" fill="currentColor"
           className="mercado-match m-6" width="68" height="68" focusable="false">
           <path d="M20.5 2h-17A1.5 1.5 0 002 3.5v17A1.5 1.5 0 003.5 22h17a1.5 1.5 0 001.5-1.5v-17A1.5 1.5 0 0020.5 2zM8 19H5v-9h3zM6.5 8.25A1.75 1.75 0 118.3 6.5a1.78 1.78 0 01-1.8 1.75zM19 19h-3v-4.74c0-1.42-.6-1.93-1.38-1.93A1.74 1.74 0 0013 14.19a.66.66 0 000 .14V19h-3v-9h2.9v1.3a3.11 3.11 0 012.7-1.4c1.55 0 3.36.86 3.36 3.66z"></path>
         </svg>
-      </div>
-
-      <NavLink
-        to={"/feed"}
-        className={({ isActive }) => `home m-6 flex flex-col items-center-safe hover:cursor-pointer ${isActive ? "active" : "hover:text-blue-700"}`}>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor"
-          className="mercado-match" width="24" height="24" focusable="false">
-          <path d="M23 9v2h-2v7a3 3 0 01-3 3h-4v-6h-4v6H6a3 3 0 01-3-3v-7H1V9l11-7 5 3.18V2h3v5.09z"></path>
-        </svg>
-        <span>Home</span>
       </NavLink>
 
       <div className="search m-6 flex flex-col items-center-safe hover:cursor-pointer">
@@ -28,8 +20,6 @@ export default function Navbar() {
       </div>
 
       <div className="mainNaviagtions flex">
-
-
         <NavLink
           to={"/feed"}
           className={({ isActive }) => `home m-6 flex flex-col items-center-safe hover:cursor-pointer ${isActive ? "active" : "hover:text-blue-700"}`}>
@@ -40,8 +30,6 @@ export default function Navbar() {
           <span>Home</span>
         </NavLink>
 
-
-
         <NavLink
           to="/network"
           className={({ isActive }) => `network m-6 flex flex-col items-center-safe hover:cursor-pointer ${isActive ? "active" : "hover:text-blue-700"}`}>
@@ -51,7 +39,6 @@ export default function Navbar() {
           <span>My Networks</span>
         </NavLink>
 
-
         <NavLink to="/jobs" className={({ isActive }) => `jobs m-6 flex flex-col items-center-safe hover:cursor-pointer ${isActive ? "active" : "hover:text-blue-700"}`}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match" width="24" height="24" focusable="false">
             <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
@@ -59,16 +46,12 @@ export default function Navbar() {
           <span>Jobs</span>
         </NavLink>
 
-
-
         <NavLink to="/messages" className={({ isActive }) => `message m-6 flex flex-col items-center-safe hover:cursor-pointer ${isActive ? "active" : "hover:text-blue-700"}`}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match" width="24" height="24" focusable="false">
             <path d="M16 4H8a7 7 0 000 14h4v4l8.16-5.39A6.78 6.78 0 0023 11a7 7 0 00-7-7zm-8 8.25A1.25 1.25 0 119.25 11 1.25 1.25 0 018 12.25zm4 0A1.25 1.25 0 1113.25 11 1.25 1.25 0 0112 12.25zm4 0A1.25 1.25 0 1117.25 11 1.25 1.25 0 0116 12.25z"></path>
           </svg>
           <span>Messaging</span>
         </NavLink>
-
-
 
         <NavLink to="/notifications" className={({ isActive }) => `notifications m-6 flex flex-col items-center-safe hover:cursor-pointer ${isActive ? "active" : "hover:text-blue-700"}`}>
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" className="mercado-match" width="24" height="24" focusable="false">
@@ -78,8 +61,6 @@ export default function Navbar() {
         </NavLink>
 
       </div>
-
-
 
       <div className="profile">
         <img
