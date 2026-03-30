e68094d02df65ecbaec146f978c05468590940c8
aakku106 <adarashagaihre80@nepathyacollege.edu.np>
2026-03-30 16:15:58 +0545

    remove random shuffling of posts and sort by creation date

--- src/features/feed/usePosts.ts
+++ src/features/feed/usePosts.ts
@@ -4,16 +4,6 @@ import { db } from "../../lib/db";
 export function usePosts() {
   return useLiveQuery(async () => {
     const posts = await db.posts.toArray();
-    const shuffledPosts = [...posts];
-
-    for (let i = shuffledPosts.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [shuffledPosts[i], shuffledPosts[j]] = [
-        shuffledPosts[j],
-        shuffledPosts[i],
-      ];
-    }
-
-    return shuffledPosts;
+    return [...posts].sort((a, b) => b.createdAt - a.createdAt);
   });
 }
